094c215faabb96ed361f8450a6fc3acc996f4a91
Zap <johan.gronvall.92@gmail.com>
2012-03-22 17:27:23 +0100

    Made additions to InteractiveObject

--- src/rsmg/model/InteractiveObject.java
+++ src/rsmg/model/InteractiveObject.java
@@ -1,19 +1,51 @@
 package rsmg.model;
+import rsmg.util.Vector2d;
 
-import java.util.Vector;
 /**
  * Class which represents an object that can interact with the main character
  * 	and has the possibility of being moved around  
- * @author Zap
+ * @author Johan Grönvall
  *
  */
 public abstract class InteractiveObject {
-	double x;
-	double y;
-	double height;
-	double width;
-	Vector velocity;
+	private double x;
+	private double y;
+	private double height;
+	private double width;
+	private Vector2d velocity;
+	
 	public InteractiveObject(double x, double y){
-		
+		this.x=x;
+		this.y=y;
 	}
+	
+	public Vector2d getVelocity(){
+		return velocity;
+	}
+	
+	public double getX(){
+		return x;
+	}
+	
+	public double getY(){
+		return y;
+	}
+	
+	public double getHeight(){
+		return height;
+	}
+	
+	public double getWidth(){
+		return width;
+	}
+	
+	public boolean hasCollidedWith(InteractiveObject obj){
+		if (obj == null){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}	
+	
 }
