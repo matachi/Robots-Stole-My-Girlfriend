13b94c4c6eeb5c705ff176c07171cfe80ec59d0e
MaTachi <m@danielj.se>
2012-03-22 17:41:15 +0100

    Done with a first version of Controller.

--- src/rsmg/controller/Controller.java
+++ src/rsmg/controller/Controller.java
@@ -1,29 +1,95 @@
 package rsmg.controller;
 
+/**
+ * 
+ * @author Daniel Jonsson
+ *
+ */
 public class Controller {
 
-	private Model model;
+	/**
+	 * If you want to show the FPS in the console.
+	 */
+	private boolean showFPSInConsole = true;
+	
+	/**
+	 * The model that is visualized on the screen and should be updated.
+	 */
+	private State state;
+
+	/**
+	 *  Necessary to calculate the time between two loops.
+	 */
+	long curLoopTime;
+	
+	/**
+	 *  Necessary if FPS is going to be calculated and shown.
+	 */
+	long nextTimeToPrintFPS;
+	long fpsCounter;
+	
 	
 	public Controller() {
+		// Let the initial state be level 1
+		state = StateFactory.getState(StateFactory.LEVEL1);
+		
+		startGameLoop();
 	}
 	
-	private void gameLoop() {
-		long startTime = System.currentTimeMillis() + 5000;
-		long fps = 0;
+	private void startGameLoop() {
+		if (showFPSInConsole)
+			runGameLoopWithFPSCounter();
+		else
+			runGameLoopWithoutFPSCounter();
+	}
+	
+	private void runGameLoopWithoutFPSCounter() {
+		initCurLoopTime();
 		
 		while (true) {
-			double delta;
-			model.update(delta);
+			calculateDelta();
 			
-			if (startTime > System.currentTimeMillis()) {
-				fps++;
-			} else {
-				long timeUsed = 5000 + (startTime - System.currentTimeMillis());
-				startTime = System.currentTimeMillis() + 5000;
-				System.out.println(fps + " frames in " + timeUsed / 1000f
-						+ " seconds = " + (fps / (timeUsed / 1000f)));
-				fps = 0;
-			}
+			state.update(delta);
+		}
+	}
+	
+	private void runGameLoopWithFPSCounter() {
+		initFPSCounterVariables();
+		initCurLoopTime();
+		
+		while (true) {
+			calculateDelta();
+			
+			calculateAndPrintFPS();
+			
+			state.update(delta);
+		}
+	}
+	
+	private void initCurLoopTime() {
+		curLoopTime = System.currentTimeMillis();
+	}
+	
+	private long calculateDelta() {
+		long prevLoopTime = curLoopTime;
+		curLoopTime = System.currentTimeMillis();
+		return curLoopTime - prevLoopTime;
+	}
+	
+	private void initFPSCounterVariables() {
+		nextTimeToPrintFPS = System.currentTimeMillis() + 5000;
+		fpsCounter = 0;
+	}
+	
+	private void calculateAndPrintFPS() {
+		if (curLoopTime < nextTimeToPrintFPS) {
+			fpsCounter++;
+		} else {
+			long timeUsed = curLoopTime - nextTimeToPrintFPS + 5000;
+			nextTimeToPrintFPS = System.currentTimeMillis() + 5000;
+			System.out.println(fpsCounter + " frames in " + timeUsed / 1000f
+					+ " seconds = " + (fpsCounter / (timeUsed / 1000f)));
+			fpsCounter = 0;
 		}
 	}
 }
