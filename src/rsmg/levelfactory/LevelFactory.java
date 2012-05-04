b32dbd49e5096068bb8d713ca2205f957de60b97
MaTachi <m@danielj.se>
2012-05-04 17:29:08 +0200

    Cleaned up LevelFactory a bit.

--- src/rsmg/levelfactory/LevelFactory.java
+++ src/rsmg/levelfactory/LevelFactory.java
@@ -34,99 +34,128 @@ import rsmg.model.tile.SpawnTile;
 import rsmg.model.tile.Tile;
 
 /**
- * Converts to/from XML to/from Tile[][]
+ * This class is responsible for creating the levels. It asks the class Levels i
+ * the io package for a level XML file, and then it converts it into a working
+ * Level.
+ * 
+ * @author Johan Rignäs, Daniel Jonsson
+ * 
  */
 public final class LevelFactory {
 
 	/**
-	 * Converts the XML file to Tile matrix by setting appropriate Tiles in the
-	 * matrix according to the XML file
+	 * Get a working, playable Level.
 	 * 
-	 * @param file
-	 *            The XML file that are going to be converted to Tiles
-	 * @return A Tile matrix. If error: null
+	 * @param levelNumber
+	 *            The level's number.
+	 * @return A Level.
 	 */
 	public static Level getLevel(int levelNumber) {
 
+		// Prepare some variables that Level's constructor will need.
 		Tile[][] grid;
 		List<Item> itemList = new ArrayList<Item>();
 		List<Ai> aiList = new ArrayList<Ai>();
 		List<Bullet> enemyBulletList = new ArrayList<Bullet>();
 		
+		// Get the document from the io package.
+		Document document = Levels.getLevel(levelNumber);
+		
+		// Get the root node.
+		Element rootNode = document.getRootElement();
 
-			Document document = Levels.getLevel(levelNumber);
-			Element rootNode = document.getRootElement();
+		// Set the size of tile tile grid.
+		Element sizeElem = rootNode.getChild("size");
+		int height = Integer.parseInt(sizeElem.getChildText("height"));
+		int width = Integer.parseInt(sizeElem.getChildText("width"));
+		grid = new Tile[height][width];
 
-			Element sizeElem = rootNode.getChild("size");
-			int height = Integer.parseInt(sizeElem.getChildText("height"));
-			int width = Integer.parseInt(sizeElem.getChildText("width"));
-			grid = new Tile[height][width];
-
+		// Walk through all row nodes in the document. These row nodes defines
+		// how the tile grid should be constructed.
+		@SuppressWarnings("unchecked")
+		List<Element> rows = rootNode.getChildren("row");
+		for (int y = 0; y < rows.size(); y++) {
+			
+			// Walk through all cells on the current row.
+			Element row = rows.get(y);
 			@SuppressWarnings("unchecked")
-			List<Element> rows = rootNode.getChildren("row");
-			for (int y = 0; y < rows.size(); y++) {
-				Element cellsElem = (Element) rows.get(y);
-				@SuppressWarnings("unchecked")
-				List<Element> cells = cellsElem.getChildren("cell");
+			List<Element> cells = row.getChildren("cell");
+			for (int x = 0; x < cells.size(); x++) {
 
-				for (int x = 0; x < cells.size(); x++) {
-
-					// Retrieve Tiles
-					Element cell = (Element) cells.get(x);
-					String cellValue = cell.getText();
-					Tile tile;
-					if (cellValue.equals("AirTile"))
+				// Retrieve tile
+				Element cell = (Element) cells.get(x);
+				String cellValue = cell.getText();
+				Tile tile;
+				switch (cellValue) {
+					case "AirTile" :
 						tile = new AirTile();
-					else if (cellValue.equals("GroundTile"))
+						break;
+					case "GroundTile" :
 						tile = new GroundTile();
-					else if (cellValue.equals("SpawnTile"))
+						break;
+					case "SpawnTile" :
 						tile = new SpawnTile();
-					else if (cellValue.equals("EndTile"))
+						break;
+					case "EndTile" :
 						tile = new EndTile();
-					else
+						break;
+					default :
 						tile = new AirTile();
-					grid[y][x] = tile;
-					
-					int scale = Constants.TILESIZE;
-					// Retrieve Items
-					String itemValue = cell.getAttributeValue("item");
-					if(itemValue != null){
-						Item item;
-						if(itemValue.equals(ObjectName.HEALTH_PACK))
+						break;
+				}
+				grid[y][x] = tile;
+				
+				int scale = Constants.TILESIZE; // Used to place items and enemies on appropriate positions.
+				// Retrieve eventual item
+				String itemValue = cell.getAttributeValue("item");
+				if (itemValue != null) {
+					Item item;
+					switch (itemValue) {
+						case ObjectName.HEALTH_PACK :
 							item = new HealthPack(x*scale,y*scale);
-						else if(itemValue.equals(ObjectName.UPGRADE_POINT))
+							break;
+						case ObjectName.UPGRADE_POINT :
 							item = new UpgradePoints(x*scale,y*scale);
-						else if(itemValue.equals(ObjectName.LASER_PISTOL))
+							break;
+						case ObjectName.LASER_PISTOL :
 							item = new Weapon(x*scale,y*scale, 15, 15, ObjectName.LASER_PISTOL);
-						else if(itemValue.equals(ObjectName.SHOTGUN))
+							break;
+						case ObjectName.SHOTGUN :
 							item = new Weapon(x*scale,y*scale, 15, 15, ObjectName.SHOTGUN);
-						else
+							break;
+						default :
 							item = new Weapon(x*scale,y*scale, 33, 15, ObjectName.ROCKET_LAUNCHER);
-						itemList.add(item);
+							break;
 					}
-					
-					// Retrieve Enemies and assign AI to them
-					String enemyValue = cell.getAttributeValue("enemy");
-					if(enemyValue != null){
-						Ai ai;
-						if(enemyValue.equals(ObjectName.TANKBOT))
+					itemList.add(item);
+				}
+				
+				// Retrieve Enemies and assign AI to them
+				String enemyValue = cell.getAttributeValue("enemy");
+				if (enemyValue != null) {
+					Ai ai;
+					switch (enemyValue) {
+						case ObjectName.TANKBOT :
 							ai = new TankBotAi(new Tankbot(x*scale, y*scale, enemyBulletList));
-						
-						else if(enemyValue.equals(ObjectName.ROCKETBOT))
+							break;
+						case ObjectName.ROCKETBOT :
 							ai = new RocketBotAi(new RocketBot(x*scale, y*scale));
-						
-						else if(enemyValue.equals(ObjectName.BALLBOT))
+							break;
+						case ObjectName.BALLBOT :
 							ai = new BallBotAi(new BallBot(x*scale, y*scale, aiList));
-						
-						else if(enemyValue.equals(ObjectName.BUCKETBOT))
+							break;
+						case ObjectName.BUCKETBOT :
 							ai = new BucketBotAi(new BucketBot(x*scale, y*scale, enemyBulletList));
-						
-						else
+							break;
+						default :
 							ai = new EmptyAi(new Spikes(x*scale, y*scale));
-						aiList.add(ai);
+							break;
 					}
+					aiList.add(ai);
 				}
 			}
+		}
+		
 		return new Level(new TileGrid(grid), itemList, aiList, enemyBulletList);
 	}
 }
