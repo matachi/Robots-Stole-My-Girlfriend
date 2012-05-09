7e56a692a65c21f29c32ead391e65018c270aa86
Johan Rignas <johan.rignas@gmail.com>
2012-05-09 22:19:24 +0200

    Updated SDD and RAD + added a test

---
+++ test/rsmg/model/ai/TestAggroRange.java
@@ -0,0 +1,21 @@
+package rsmg.model.ai;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import rsmg.model.object.unit.Enemy;
+import rsmg.model.object.unit.PCharacter;
+import rsmg.model.object.unit.Tankbot;
+
+public class TestAggroRange {
+	
+	@Test
+	public void testCharInAggroRange(){
+		PCharacter character = new PCharacter(4, 4, null);
+		Enemy enemy = new Tankbot(5,5, null);
+		
+		assertTrue(AggroRange.charInAggroRange(character.getX(), character.getY(), enemy, 2));
+	}
+}
+
---
+++ test/rsmg/model/ai/TestBallBotAi.java
@@ -0,0 +1,32 @@
+package rsmg.model.ai;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestBallBotAi {
+	
+	@Before
+	public void before() {
+		
+	}
+	
+	@Test
+	public void testUpdate() {
+		
+	}
+
+	// Private
+	@Test
+	public void testShouldShoot() {	}
+	
+	// Private
+	@Test
+	public void testSpawnEnemy() {}
+
+	@Test
+	public void testGetEnemy() {
+		
+	}
+}
+
